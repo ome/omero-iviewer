390d49cbf286a46dfccb2904480a3b5ba5b47b05
Harald Waxenegger <harald.waxenegger@gmail.com>
2017-03-29 10:16:49 +1000

    retrieve regions/shapes data via roi service & omero marshal

--- plugin/ol3-viewer/urls.py
+++ plugin/ol3-viewer/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('django.views.generic.simple',
                        url(r'^plugin/(?P<iid>[0-9]+)?/?$', views.plugin,
                            name='ol3-viewer-plugin'),
                        url(r'^plugin-debug/(?P<iid>[0-9]+)?/?$',
-                           views.plugin_debug, name='ol3-viewer-plugin-debug'))
+                           views.plugin_debug, name='ol3-viewer-plugin-debug'),
+                       url(r'^request_rois/(?P<iid>[0-9]+)?/?$',
+                           views.request_rois, name='ol3-viewer-request-rois'))
--- plugin/ol3-viewer/views.py
+++ plugin/ol3-viewer/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from omeroweb.decorators import login_required
 from django.http import HttpResponse
+
+import omero_marshal
+from omero.model import MaskI
 
 
 @login_required()
@@ -25,3 +29,36 @@ def plugin(request, iid=None, conn=None, debug=False, **kwargs):
 @login_required()
 def plugin_debug(request, iid=None, conn=None, **kwargs):
     return plugin(request, iid=iid, conn=conn, debug=True)
+
+
+@login_required()
+def request_rois(request, iid, conn=None, **kwargs):
+    if iid is None:
+        return JsonResponse({"error":
+                            "no image id supplied for regions request"})
+
+    roi_service = conn.getRoiService()
+    if roi_service is None:
+        return JsonResponse({"error":
+                            "Could not get rois instance of roi service!"})
+
+    ret = []
+    try:
+        result = roi_service.findByImage(long(iid), None)
+        for roi in result.rois:
+            rois_to_be_returned = {"@id": roi.getId().val, "shapes": []}
+            for shape in roi.copyShapes():
+                # masks not supported by marshal (would raise exception)
+                if shape.__class__ == MaskI:
+                    continue
+                encoder = omero_marshal.get_encoder(shape.__class__)
+                encoded_shape = encoder.encode(shape)
+                if encoded_shape is None:
+                    return JsonResponse({"error": "Failed to encode roi!"})
+                rois_to_be_returned['shapes'].append(encoded_shape)
+                ret.append(rois_to_be_returned)
+
+        return JsonResponse({"rois": ret})
+    except Exception as someException:
+        return JsonResponse({"error": "Failed to request/marshal rois: " +
+                            repr(someException)})
