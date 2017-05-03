8336d4aa94fd3600c23c932a8d66320010d7b3f9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-05-03 14:55:14 +0100

    Do no display max value

--- src/controls/dimension-slider.js
+++ src/controls/dimension-slider.js
@@ -1,3 +1,20 @@
+//
+// Copyright (C) 2017 University of Dundee & Open Microscopy Environment.
+// All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 //css and images
 require('../../node_modules/jquery-ui/themes/base/slider.css');
 
@@ -225,10 +242,7 @@ export default class DimensionSlider extends EventSubscriber {
         let imgInf = this.image_config.image_info;
         let oldValue = imgInf.dimensions[this.dim];
 
-        // show new value
-        $('.slider-corner .' + this.dim).text(
-            this.dim.toUpperCase() + ":" + (value+1) + "/" +
-            imgInf.dimensions['max_' + this.dim]);
+        $('.slider-corner .' + this.dim).text(value+1);
 
         // no need to change for a the same value
         if (slider_interaction ||
@@ -292,10 +306,7 @@ export default class DimensionSlider extends EventSubscriber {
             change: (event, ui) => this.onChange(ui.value,
                 event.originalEvent ? true : false)
         });
-        $('.slider-corner .' + this.dim).text(
-            this.dim.toUpperCase() + ":" +
-            (imgInf.dimensions[this.dim]+1) + "/" +
-                imgInf.dimensions['max_' + this.dim]);
+        $('.slider-corner .' + this.dim).text(imgInf.dimensions[this.dim]+1);
         this.show();
     }
 
