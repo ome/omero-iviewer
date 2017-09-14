e8cd8b89669d7bc9d6313984d127702b94fb4798
Harald Waxenegger <harald.waxenegger@gmail.com>
2017-09-14 12:36:32 +1000

    forgot karma.conf

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: ['test/build/ol3-viewer-test.js','test/unit/**/*.js'],
+    exclude: ['test/unit/debug_mocha.js'],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
+    concurrency: Infinity
+  })
+}
