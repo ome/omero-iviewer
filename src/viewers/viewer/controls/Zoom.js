71a6fa357ad7271a64639aa2c475918511877da2
William Moore <w.moore@dundee.ac.uk>
2019-01-16 04:58:20 +0000

    Refactor controls/Zoom.js to ES6 class syntax

--- src/viewers/viewer/controls/Zoom.js
+++ src/viewers/viewer/controls/Zoom.js
@@ -21,7 +21,6 @@ import EventType from 'ol/events/EventType';
 import Control from 'ol/control/Control';
 import {CLASS_UNSELECTABLE, CLASS_CONTROL } from 'ol/css';
 import {easeOut} from 'ol/easing';
-import {inherits} from 'ol/util';
 import {getTargetId} from '../utils/Misc';
 
 /**
@@ -29,212 +28,214 @@ import {getTargetId} from '../utils/Misc';
  * A custom zoom control that displays the zoom with the possibility to
  * enter a number
  *
- * @constructor
  * @extends {ol.control.Control}
- * @param {olx.control.ZoomOptions=} opt_options Zoom options.
  */
-const Zoom = function(opt_options) {
-    var options = opt_options ? opt_options : {};
+class Zoom extends Control {
 
     /**
-    * @type {number}
-    * @private
-    */
-    this.duration_ = options.duration !== undefined ? options.duration : 0;
+     * @constructor
+     * @param {olx.control.ZoomOptions=} opt_options Zoom options.
+     */
+    constructor(opt_options) {
+        var options = opt_options ? opt_options : {};
+
+        var element = document.createElement('div');
+        super({
+            element: element,
+            target: options.target
+        });
+
+        /**
+        * @type {number}
+        * @private
+        */
+        this.duration_ = options.duration !== undefined ? options.duration : 0;
+
+        /**
+        * @type {number}
+        * @private
+        */
+        this.delta_ = options.delta === 'number' ? options.delta : 1;
+
+        /**
+        * @type {string}
+        * @private
+        */
+        this.class_name_ =
+            options.className === 'string' ? options.className : 'ol-zoom';
+
+        var cssClasses =
+            this.class_name_ + ' ' + CLASS_UNSELECTABLE + ' ' +
+                CLASS_CONTROL;
+
+        element.className = cssClasses;
+        var buttonGroup = document.createElement('div');
+        buttonGroup.className = "btn-group btn-group-sm ol-zoom-buttons";
+        buttonGroup.appendChild(this.addZoomButton_(false));
+        buttonGroup.appendChild(this.addZoomButton_(true));
+        buttonGroup.appendChild(this.addFitToExtentButton_());
+        buttonGroup.appendChild(this.addOneToOneButton_());
+        element.appendChild(buttonGroup);
+        element.appendChild(this.addZoomPercentage_());
+    }
 
     /**
-    * @type {number}
-    * @private
-    */
-    this.delta_ = options.delta === 'number' ? options.delta : 1;
+     * Adds both, zoom in and out buttons
+     * @param {boolean} zoom_in the in zoom button is added if true, otherwise out
+     * @private
+     */
+    addZoomButton_(zoom_in) {
+        if (typeof zoom_in !== 'boolean') zoom_in = false;
+
+        var title = 'Zoom ' + (zoom_in ? 'in' : 'out');
+        var element = document.createElement('button');
+        element.className =
+            this.class_name_ + (zoom_in ? '-in' : '-out') +
+            " btn btn-default glyphicon " +
+            (zoom_in ? 'glyphicon-plus' : 'glyphicon-minus');
+        element.setAttribute('type', 'button');
+        element.title = title;
+
+        listen(element, EventType.CLICK, this.handleClick_, this);
+
+        return element;
+    }
 
     /**
-    * @type {string}
-    * @private
-    */
-    this.class_name_ =
-        options.className === 'string' ? options.className : 'ol-zoom';
+     * Adds an input field for entering and displaying zoom values (in percent)
+     * @private
+     */
+    addZoomPercentage_() {
+        var zoomDisplayElement = document.createElement('input');
+        zoomDisplayElement.className = this.class_name_ + '-display';
+        zoomDisplayElement.setAttribute('type', 'input');
+        zoomDisplayElement.className = 'form-control ol-zoom-display';
+        listen(
+            zoomDisplayElement, "keyup",
+            function(event) {
+                if (event.keyCode === 13)
+                    this.changeResolution_(parseInt(zoomDisplayElement.value));
+            },this);
 
-    var cssClasses =
-        this.class_name_ + ' ' + CLASS_UNSELECTABLE + ' ' +
-            CLASS_CONTROL;
-
-    var element = document.createElement('div');
-    element.className = cssClasses;
-    var buttonGroup = document.createElement('div');
-    buttonGroup.className = "btn-group btn-group-sm ol-zoom-buttons";
-    buttonGroup.appendChild(this.addZoomButton_(false));
-    buttonGroup.appendChild(this.addZoomButton_(true));
-    buttonGroup.appendChild(this.addFitToExtentButton_());
-    buttonGroup.appendChild(this.addOneToOneButton_());
-    element.appendChild(buttonGroup);
-    element.appendChild(this.addZoomPercentage_());
-
-    Control.call(this, {
-        element: element,
-        target: options.target
-    });
-};
-inherits(Zoom, Control);
-
-/**
- * Adds both, zoom in and out buttons
- * @param {boolean} zoom_in the in zoom button is added if true, otherwise out
- * @private
- */
-Zoom.prototype.addZoomButton_ = function(zoom_in) {
-    if (typeof zoom_in !== 'boolean') zoom_in = false;
-
-    var title = 'Zoom ' + (zoom_in ? 'in' : 'out');
-    var element = document.createElement('button');
-    element.className =
-        this.class_name_ + (zoom_in ? '-in' : '-out') +
-        " btn btn-default glyphicon " +
-        (zoom_in ? 'glyphicon-plus' : 'glyphicon-minus');
-    element.setAttribute('type', 'button');
-    element.title = title;
-
-    listen(element, EventType.CLICK,
-        Zoom.prototype.handleClick_,
-        this);
-
-    return element;
-};
-
-/**
- * Adds an input field for entering and displaying zoom values (in percent)
- * @private
- */
-Zoom.prototype.addZoomPercentage_ = function() {
-    var zoomDisplayElement = document.createElement('input');
-    zoomDisplayElement.className = this.class_name_ + '-display';
-    zoomDisplayElement.setAttribute('type', 'input');
-    zoomDisplayElement.className = 'form-control ol-zoom-display';
-    listen(
-        zoomDisplayElement, "keyup",
-        function(event) {
-            if (event.keyCode === 13)
+        var percent = document.createElement('button');
+        percent.className = 'btn btn-default ol-zoom-percent';
+        percent.title = 'Click to apply Zoom Value';
+        percent.appendChild(document.createTextNode("%"));
+        listen(
+            percent, "click",
+            function() {
                 this.changeResolution_(parseInt(zoomDisplayElement.value));
-        },this);
+            },this);
 
-    var percent = document.createElement('button');
-    percent.className = 'btn btn-default ol-zoom-percent';
-    percent.title = 'Click to apply Zoom Value';
-    percent.appendChild(document.createTextNode("%"));
-    listen(
-        percent, "click",
-        function() {
-            this.changeResolution_(parseInt(zoomDisplayElement.value));
-        },this);
+        var span = document.createElement('span');
+        span.className = "input-group-btn input-group-sm";
+        span.appendChild(percent);
+        var element = document.createElement('div');
+        element.className = "input-group ol-zoom-percentage";
+        element.appendChild(zoomDisplayElement);
+        element.appendChild(span);
 
-    var span = document.createElement('span');
-    span.className = "input-group-btn input-group-sm";
-    span.appendChild(percent);
-    var element = document.createElement('div');
-    element.className = "input-group ol-zoom-percentage";
-    element.appendChild(zoomDisplayElement);
-    element.appendChild(span);
-
-    return element;
-}
-
-/**
- * Changes the resolution to the given value
- * @param {number} value the new value
- * @private
- */
-Zoom.prototype.changeResolution_ = function(value) {
-    var map = this.getMap();
-    var view = map ? map.getView() : null;
-    if (view === null) return;
-
-    if (!isNaN(value)) {
-        if (value < 0 ) value = 0;
-        var constrainedResolution =
-            view.constrainResolution(1 / (value / 100), 0, 0);
-            view.setResolution(constrainedResolution);
+        return element;
     }
-    var targetId = getTargetId(map.getTargetElement());
-    var zoomDisplayElement =
-        document.getElementById('' + targetId).querySelectorAll(
-            '.ol-zoom-display')
-    if (zoomDisplayElement.length === 0) return;
-    zoomDisplayElement[0].value = Math.round((1 / view.getResolution()) * 100);
-}
 
-/**
- * Adds a 1:1 zoom button
- * @private
- */
-Zoom.prototype.addOneToOneButton_ = function() {
-    var oneToOneElement = document.createElement('button');
-    oneToOneElement.className = this.class_name_ + '-1-1' + " btn btn-default";
-    oneToOneElement.setAttribute('type', 'button');
-    oneToOneElement.title = "Actual Size";
-    oneToOneElement.appendChild(document.createTextNode("1:1"));
-    listen(oneToOneElement, EventType.CLICK,
-        function() {
-            var map = this.getMap();
-            var view = map ? map.getView() : null;
-            if (view === null) return;
-            view.setResolution(1);
-            var ext = view.getProjection().getExtent();
-            view.setCenter([(ext[2]-ext[0])/2, -(ext[3]-ext[1])/2]);
-        }, this);
+    /**
+     * Changes the resolution to the given value
+     * @param {number} value the new value
+     * @private
+     */
+    changeResolution_(value) {
+        var map = this.getMap();
+        var view = map ? map.getView() : null;
+        if (view === null) return;
 
-    return oneToOneElement;
-};
-
-/**
- * Adds a zoom button that will make the image fit into the viewing extent
- * @private
- */
-Zoom.prototype.addFitToExtentButton_ = function() {
-    var oneToOneElement = document.createElement('button');
-    oneToOneElement.className =
-        this.class_name_ + '-fit' +
-        " btn btn-default glyphicon glyphicon-fullscreen";
-    oneToOneElement.setAttribute('type', 'button');
-    oneToOneElement.title = "Zoom Image to Fit";
-    listen(oneToOneElement, EventType.CLICK,
-        function() {
-            var map = this.getMap();
-            var view = map ? map.getView() : null;
-            if (view === null) return;
-
-            var ext = view.getProjection().getExtent();
-            view.fit([ext[0], -ext[3], ext[2], ext[1]]);
-        }, this);
-
-    return oneToOneElement;
-};
-
-/**
- * @param {Event} event The event to handle
- * @private
- */
-Zoom.prototype.handleClick_ = function(event) {
-    event.preventDefault();
-
-    var map = this.getMap();
-    var view = map.getView();
-    if (!view) return;
-
-    var delta = event.target.className.indexOf("ol-zoom-out") !== -1 ? -1 : 1;
-    var currentResolution = view.getResolution();
-    if (currentResolution) {
-        var newResolution = view.constrainResolution(currentResolution, delta);
-        if (this.duration_ > 0) {
-            if (view.getAnimating()) {
-              view.cancelAnimations();
-            }
-            view.animate({
-              resolution: newResolution,
-              duration: this.duration_,
-              easing: easeOut
-            });
-        } else view.setResolution(newResolution);
+        if (!isNaN(value)) {
+            if (value < 0 ) value = 0;
+            var constrainedResolution =
+                view.constrainResolution(1 / (value / 100), 0, 0);
+                view.setResolution(constrainedResolution);
+        }
+        var targetId = getTargetId(map.getTargetElement());
+        var zoomDisplayElement =
+            document.getElementById('' + targetId).querySelectorAll(
+                '.ol-zoom-display')
+        if (zoomDisplayElement.length === 0) return;
+        zoomDisplayElement[0].value = Math.round((1 / view.getResolution()) * 100);
     }
-};
+
+    /**
+     * Adds a 1:1 zoom button
+     * @private
+     */
+    addOneToOneButton_() {
+        var oneToOneElement = document.createElement('button');
+        oneToOneElement.className = this.class_name_ + '-1-1' + " btn btn-default";
+        oneToOneElement.setAttribute('type', 'button');
+        oneToOneElement.title = "Actual Size";
+        oneToOneElement.appendChild(document.createTextNode("1:1"));
+        listen(oneToOneElement, EventType.CLICK,
+            function() {
+                var map = this.getMap();
+                var view = map ? map.getView() : null;
+                if (view === null) return;
+                view.setResolution(1);
+                var ext = view.getProjection().getExtent();
+                view.setCenter([(ext[2]-ext[0])/2, -(ext[3]-ext[1])/2]);
+            }, this);
+
+        return oneToOneElement;
+    }
+
+    /**
+     * Adds a zoom button that will make the image fit into the viewing extent
+     * @private
+     */
+    addFitToExtentButton_() {
+        var oneToOneElement = document.createElement('button');
+        oneToOneElement.className =
+            this.class_name_ + '-fit' +
+            " btn btn-default glyphicon glyphicon-fullscreen";
+        oneToOneElement.setAttribute('type', 'button');
+        oneToOneElement.title = "Zoom Image to Fit";
+        listen(oneToOneElement, EventType.CLICK,
+            function() {
+                var map = this.getMap();
+                var view = map ? map.getView() : null;
+                if (view === null) return;
+
+                var ext = view.getProjection().getExtent();
+                view.fit([ext[0], -ext[3], ext[2], ext[1]]);
+            }, this);
+
+        return oneToOneElement;
+    };
+
+    /**
+     * @param {Event} event The event to handle
+     * @private
+     */
+    handleClick_(event) {
+        event.preventDefault();
+
+        var map = this.getMap();
+        var view = map.getView();
+        if (!view) return;
+
+        var delta = event.target.className.indexOf("ol-zoom-out") !== -1 ? -1 : 1;
+        var currentResolution = view.getResolution();
+        if (currentResolution) {
+            var newResolution = view.constrainResolution(currentResolution, delta);
+            if (this.duration_ > 0) {
+                if (view.getAnimating()) {
+                view.cancelAnimations();
+                }
+                view.animate({
+                resolution: newResolution,
+                duration: this.duration_,
+                easing: easeOut
+                });
+            } else view.setResolution(newResolution);
+        }
+    }
+}
 
 export default Zoom;
