4957be05fcf16f14dea39280df2d3d07658798d4
William Moore <w.moore@dundee.ac.uk>
2019-01-16 05:00:08 +0000

    Refactor geom/Point.js to ES6 class syntax

--- src/viewers/viewer/geom/Point.js
+++ src/viewers/viewer/geom/Point.js
@@ -22,97 +22,99 @@ import {applyTransform,
     convertMatrixToAffineTransform,
     convertAffineTransformIntoMatrix} from '../utils/Transform';
 import {isArray} from '../utils/Misc';
-import {inherits} from 'ol/util';
 
 /**
  * @classdesc
  * Point extends of the built-in ol.geom.Circle
  *
- * @constructor
  * @extends {ol.geom.Circle}
- *
- * @param {Array.<number>} coords the point coordinates
- * @param {Object=} transform an AffineTransform object according to omero marshal
  */
-const Point = function(coords, transform) {
-    // preliminary checks: are all mandatory paramters numeric
-    if (!isArray(coords) || coords.length !== 2)
-        console.error("Point needs an array of coordinates (length: 2)!");
+class Point extends Circle {
 
     /**
-     * the size of the point (radius of circle that is)
-     * @type {Array.<number>}
-     * @private
+     * @constructor
+     *
+     * @param {Array.<number>} coords the point coordinates
+     * @param {Object=} transform an AffineTransform object according to omero marshal
      */
-    this.radius_ = 5;
+    constructor(coords, transform) {
+        // preliminary checks: are all mandatory paramters numeric
+        if (!isArray(coords) || coords.length !== 2)
+            console.error("Point needs an array of coordinates (length: 2)!");
 
-    /**
-     * the initial coordinates as a flat array
-     * @type {Array.<number>}
-     * @private
-     */
-    this.initial_coords_ = null;
+        const radius = 5;
+        super(coords, radius);
 
-    /**
-     * the transformation matrix of length 6
-     * @type {Array.<number>|null}
-     * @private
-     */
-    this.transform_ = convertAffineTransformIntoMatrix(transform);
+        /**
+         * the size of the point (radius of circle that is)
+         * @type {Array.<number>}
+         * @private
+         */
+        this.radius_ = radius;
 
-    // call super, handing in our coordinates and radius
-    // goog.base(this, coords, this.radius_);
-    Circle.call(this, coords, this.radius_);
-    this.initial_coords_ = this.getFlatCoordinates();
+        /**
+         * the initial coordinates as a flat array
+         * @type {Array.<number>}
+         * @private
+         */
+        this.initial_coords_ = null;
 
-    // apply potential transform
-    this.flatCoordinates = applyTransform(
-            this.transform_, this.initial_coords_);
-}
-inherits(Point, Circle);
+        /**
+         * the transformation matrix of length 6
+         * @type {Array.<number>|null}
+         * @private
+         */
+        this.transform_ = convertAffineTransformIntoMatrix(transform);
 
+        this.initial_coords_ = this.getFlatCoordinates();
 
-/**
- * Returns the coordinates as a flat array (excl. any potential transform)
- * @return {Array.<number>} the coordinates as a flat array
- */
-Point.prototype.getPointCoordinates = function() {
-    var ret =
-        this.transform_ ? this.initial_coords_ : this.getFlatCoordinates();
-    return ret.slice(0, 2);
-}
-
-/**
- * Gets the transformation associated with the point
- * @return {Object|null} the AffineTransform object (omero marshal) or null
- */
-Point.prototype.getTransform = function() {
-    return convertMatrixToAffineTransform(this.transform_);
-}
-
-/**
- * First translate then store the newly translated coords
- *
- * @private
- */
-Point.prototype.translate = function(deltaX, deltaY) {
-    // delegate
-    if (this.transform_) {
-        this.transform_[4] += deltaX;
-        this.transform_[5] -= deltaY;
+        // apply potential transform
         this.flatCoordinates = applyTransform(
                 this.transform_, this.initial_coords_);
-        this.changed();
-    } else SimpleGeometry.prototype.translate.call(this, deltaX, deltaY);
-};
+    }
 
-/**
- * Make a complete copy of the geometry.
- * @return {Point} Clone.
- */
-Point.prototype.clone = function() {
-    return new Point(
-        this.getPointCoordinates(), this.getTransform());
-};
+    /**
+     * Returns the coordinates as a flat array (excl. any potential transform)
+     * @return {Array.<number>} the coordinates as a flat array
+     */
+    getPointCoordinates() {
+        var ret =
+            this.transform_ ? this.initial_coords_ : this.getFlatCoordinates();
+        return ret.slice(0, 2);
+    }
+
+    /**
+     * Gets the transformation associated with the point
+     * @return {Object|null} the AffineTransform object (omero marshal) or null
+     */
+    getTransform() {
+        return convertMatrixToAffineTransform(this.transform_);
+    }
+
+    /**
+     * First translate then store the newly translated coords
+     *
+     * @private
+     */
+    translate(deltaX, deltaY) {
+        // delegate
+        if (this.transform_) {
+            this.transform_[4] += deltaX;
+            this.transform_[5] -= deltaY;
+            this.flatCoordinates = applyTransform(
+                    this.transform_, this.initial_coords_);
+            this.changed();
+        } else SimpleGeometry.prototype.translate.call(this, deltaX, deltaY);
+    }
+
+    /**
+     * Make a complete copy of the geometry.
+     * @return {Point} Clone.
+     */
+    clone() {
+        return new Point(
+            this.getPointCoordinates(), this.getTransform());
+    }
+}
 
 export default Point;
