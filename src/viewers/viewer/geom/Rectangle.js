78a50060d54ade231d25316c0bb25f2929249145
William Moore <w.moore@dundee.ac.uk>
2019-01-16 05:00:25 +0000

    Refactor geom/Rectangle.js to ES6 class syntax

--- src/viewers/viewer/geom/Rectangle.js
+++ src/viewers/viewer/geom/Rectangle.js
@@ -19,7 +19,6 @@
 import Polygon from 'ol/geom/Polygon';
 import SimpleGeometry from 'ol/geom/SimpleGeometry';
 import GeometryLayout from 'ol/geom/GeometryLayout';
-import {inherits} from 'ol/util';
 import {isArray} from '../utils/Misc';
 import {getLength} from '../utils/Regions';
 import {applyTransform,
@@ -34,208 +33,210 @@ import {applyTransform,
  * which is a polygon to represent the rectangle. Note that the given point
  * represents the upper left corner!
  *
- * @constructor
  * @extends {ol.geom.Polygon}
- *
- * @param {number} x the x coordinate of the upper left corner
- * @param {number} y the y coordinate of the upper left corner
- * @param {number} w the width of the rectangle
- * @param {number} h the height of the rectangle
- * @param {Object=} transform an AffineTransform object according to omero marshal
  */
-const Rectangle = function(x, y, w, h, transform) {
-    // preliminary checks: set sensible defaults if violated
-    if (typeof x !== 'number') x = 0;
-    if (typeof y !== 'number') y = 0;
-    if (typeof w !== 'number' || w <= 0) w = 1;
-    if (typeof h !== 'number' || h <= 0) h = 1;
+class Rectangle extends Polygon {
 
     /**
-     * the initial coordinates as a flat array
-     * @type {Array.<number>}
-     * @private
+     * @constructor
+     *
+     * @param {number} x the x coordinate of the upper left corner
+     * @param {number} y the y coordinate of the upper left corner
+     * @param {number} w the width of the rectangle
+     * @param {number} h the height of the rectangle
+     * @param {Object=} transform an AffineTransform object according to omero marshal
      */
-    this.initial_coords_ = null;
+    constructor(x, y, w, h, transform) {
+        // preliminary checks: set sensible defaults if violated
+        if (typeof x !== 'number') x = 0;
+        if (typeof y !== 'number') y = 0;
+        if (typeof w !== 'number' || w <= 0) w = 1;
+        if (typeof h !== 'number' || h <= 0) h = 1;
 
-    // set the rectangle coordinates
-    var coords = [[
-        [x, y],
-        [x+w, y],
-        [x+w, y-h],
-        [x, y-h],
-        [x, y]
-    ]];
+        // set the rectangle coordinates
+        var coords = [[
+            [x, y],
+            [x+w, y],
+            [x+w, y-h],
+            [x, y-h],
+            [x, y]
+        ]];
+        super(coords, GeometryLayout.XY);
 
-    /**
-     * the transformation matrix of length 6
-     * @type {Array.<number>|null}
-     * @private
-     */
-    this.transform_ = convertAffineTransformIntoMatrix(transform);
+        /**
+         * the initial coordinates as a flat array
+         * @type {Array.<number>}
+         * @private
+         */
+        this.initial_coords_ = null;
 
-    // call super and hand in our coordinate array
-    // goog.base(this, coords, GeometryLayout.XY);
-    Polygon.call(this, coords, GeometryLayout.XY);
-    this.initial_coords_ =  this.getFlatCoordinates();
+        /**
+         * the transformation matrix of length 6
+         * @type {Array.<number>|null}
+         * @private
+         */
+        this.transform_ = convertAffineTransformIntoMatrix(transform);
 
-    // apply potential transform
-    this.flatCoordinates = applyTransform(
-            this.transform_, this.initial_coords_);
-}
-inherits(Rectangle, Polygon);
+        this.initial_coords_ =  this.getFlatCoordinates();
 
-/**
- * Gets the upper left corner coordinates as an array [x,y]
- * @return {Array.<number>} the upper left corner
- */
-Rectangle.prototype.getUpperLeftCorner = function() {
-    var flatCoords = this.getRectangleCoordinates();
-    if (!isArray(flatCoords) || flatCoords.length != 10)
-        return null;
-
-    return [flatCoords[0], flatCoords[1]];
-}
-
-/**
- * sets the upper left corner using a coordinate array [x,y]
- *
- * @param {Array.<number>} value upper left corner
- */
-Rectangle.prototype.setUpperLeftCorner = function(value) {
-    if (!isArray(value)) return;
-    this.changeRectangle(value[0], value[1]);
-}
-
-/**
- * Gets the width of the rectangle
- * @return {number} the width of the rectangle
- */
-Rectangle.prototype.getWidth = function() {
-    var flatCoords = this.getRectangleCoordinates();
-    if (!isArray(flatCoords) || flatCoords.length != 10)
-        return 0;
-
-    return flatCoords[2]-flatCoords[0];
-}
-
-/**
- * Sets the width of the rectangle
- *
- * @param {number} value the width of the rectangle
- */
-Rectangle.prototype.setWidth = function(value) {
-    this.changeRectangle(null,null,value, null);
-}
-
-/**
- * Gets the height of the rectangle
- * @return {number} the height of the rectangle
- */
-Rectangle.prototype.getHeight = function() {
-    var flatCoords = this.getRectangleCoordinates();
-    if (!isArray(flatCoords) || flatCoords.length != 10)
-        return 0;
-
-    return Math.abs(flatCoords[5]-flatCoords[3]);
-}
-
-/**
- * Sets the height of the rectangle
- *
- * @param {number} value the height of the rectangle
- */
-Rectangle.prototype.setHeight = function(value) {
-    this.changeRectangle(null,null,null, value);
-}
-
-/**
- * Changes the coordinates/dimensions of an existing rectangle.
- * Used internally.
- *
- * @private
- * @param {number} x the x coordinate of the upper left corner
- * @param {number} y the y coordinate of the upper left corner
- * @param {number} w the width of the rectangle
- * @param {number} h the height of the rectangle
- */
-Rectangle.prototype.changeRectangle = function(x,y,w,h) {
-    var flatCoords = this.getRectangleCoordinates();
-    if (!isArray(flatCoords) || flatCoords.length != 10)
-        return;
-
-    if (typeof(x) !== 'number') x = flatCoords[0];
-    if (typeof(y) !== 'number') y = flatCoords[1];
-    if (typeof(w) !== 'number') w = this.getWidth();
-    if (typeof(h) !== 'number') h = this.getHeight();
-
-    var coords = [[
-        [x, y],
-        [x+w, y],
-        [x+w, y-h],
-        [x, y-h],
-        [x, y]
-    ]];
-    this.setCoordinates(coords);
-}
-
-/**
- * Override translation to take care of possible transformation
- *
- * @private
- */
-Rectangle.prototype.translate = function(deltaX, deltaY) {
-    // delegate
-    if (this.transform_) {
-        this.transform_[4] += deltaX;
-        this.transform_[5] -= deltaY;
+        // apply potential transform
         this.flatCoordinates = applyTransform(
                 this.transform_, this.initial_coords_);
-        this.changed();
-    } else SimpleGeometry.prototype.translate.call(this, deltaX, deltaY);
-};
+    }
 
-/**
- * Turns the tansformation matrix back into the ome model object
- * @return {Object|null} the ome model transformation
- */
-Rectangle.prototype.getTransform = function() {
-    if (!isArray(this.transform_)) return null;
+    /**
+     * Gets the upper left corner coordinates as an array [x,y]
+     * @return {Array.<number>} the upper left corner
+     */
+    getUpperLeftCorner() {
+        var flatCoords = this.getRectangleCoordinates();
+        if (!isArray(flatCoords) || flatCoords.length != 10)
+            return null;
 
-    return {'@type': "http://www.openmicroscopy.org/Schemas/OME/2016-06#AffineTransform",
-            'A00' : this.transform_[0], 'A10' : this.transform_[1],
-            'A01' : this.transform_[2], 'A11' : this.transform_[3],
-            'A02' : this.transform_[4], 'A12' : this.transform_[5]
+        return [flatCoords[0], flatCoords[1]];
+    }
+
+    /**
+     * sets the upper left corner using a coordinate array [x,y]
+     *
+     * @param {Array.<number>} value upper left corner
+     */
+    setUpperLeftCorner(value) {
+        if (!isArray(value)) return;
+        this.changeRectangle(value[0], value[1]);
+    }
+
+    /**
+     * Gets the width of the rectangle
+     * @return {number} the width of the rectangle
+     */
+    getWidth() {
+        var flatCoords = this.getRectangleCoordinates();
+        if (!isArray(flatCoords) || flatCoords.length != 10)
+            return 0;
+
+        return flatCoords[2]-flatCoords[0];
+    }
+
+    /**
+     * Sets the width of the rectangle
+     *
+     * @param {number} value the width of the rectangle
+     */
+    setWidth(value) {
+        this.changeRectangle(null,null,value, null);
+    }
+
+    /**
+     * Gets the height of the rectangle
+     * @return {number} the height of the rectangle
+     */
+    getHeight() {
+        var flatCoords = this.getRectangleCoordinates();
+        if (!isArray(flatCoords) || flatCoords.length != 10)
+            return 0;
+
+        return Math.abs(flatCoords[5]-flatCoords[3]);
+    }
+
+    /**
+     * Sets the height of the rectangle
+     *
+     * @param {number} value the height of the rectangle
+     */
+    setHeight(value) {
+        this.changeRectangle(null,null,null, value);
+    }
+
+    /**
+     * Changes the coordinates/dimensions of an existing rectangle.
+     * Used internally.
+     *
+     * @private
+     * @param {number} x the x coordinate of the upper left corner
+     * @param {number} y the y coordinate of the upper left corner
+     * @param {number} w the width of the rectangle
+     * @param {number} h the height of the rectangle
+     */
+    changeRectangle(x,y,w,h) {
+        var flatCoords = this.getRectangleCoordinates();
+        if (!isArray(flatCoords) || flatCoords.length != 10)
+            return;
+
+        if (typeof(x) !== 'number') x = flatCoords[0];
+        if (typeof(y) !== 'number') y = flatCoords[1];
+        if (typeof(w) !== 'number') w = this.getWidth();
+        if (typeof(h) !== 'number') h = this.getHeight();
+
+        var coords = [[
+            [x, y],
+            [x+w, y],
+            [x+w, y-h],
+            [x, y-h],
+            [x, y]
+        ]];
+        this.setCoordinates(coords);
+    }
+
+    /**
+     * Override translation to take care of possible transformation
+     *
+     * @private
+     */
+    translate(deltaX, deltaY) {
+        // delegate
+        if (this.transform_) {
+            this.transform_[4] += deltaX;
+            this.transform_[5] -= deltaY;
+            this.flatCoordinates = applyTransform(
+                    this.transform_, this.initial_coords_);
+            this.changed();
+        } else SimpleGeometry.prototype.translate.call(this, deltaX, deltaY);
     };
-}
 
-/**
- * Returns the coordinates as a flat array (excl. any potential transform)
- * @return {Array.<number>} the coordinates as a flat array
- */
-Rectangle.prototype.getRectangleCoordinates = function() {
-    return (
-        this.transform_ ? this.initial_coords_ : this.getFlatCoordinates()
-    );
-}
+    /**
+     * Turns the tansformation matrix back into the ome model object
+     * @return {Object|null} the ome model transformation
+     */
+    getTransform() {
+        if (!isArray(this.transform_)) return null;
 
-/**
- * Make a complete copy of the geometry.
- * @return {Rectangle} Clone.
- */
-Rectangle.prototype.clone = function() {
-    var topLeft = this.getUpperLeftCorner();
-    return new Rectangle(
-        topLeft[0], topLeft[1], this.getWidth(), this.getHeight(),
-        this.getTransform());
-};
+        return {'@type': "http://www.openmicroscopy.org/Schemas/OME/2016-06#AffineTransform",
+                'A00' : this.transform_[0], 'A10' : this.transform_[1],
+                'A01' : this.transform_[2], 'A11' : this.transform_[3],
+                'A02' : this.transform_[4], 'A12' : this.transform_[5]
+        };
+    }
 
-/**
- * Returns the length of the rectangle
- *
- * @return {number} the length of the rectangle
- */
-Rectangle.prototype.getLength = function() {
-    return getLength(this);
+    /**
+     * Returns the coordinates as a flat array (excl. any potential transform)
+     * @return {Array.<number>} the coordinates as a flat array
+     */
+    getRectangleCoordinates() {
+        return (
+            this.transform_ ? this.initial_coords_ : this.getFlatCoordinates()
+        );
+    }
+
+    /**
+     * Make a complete copy of the geometry.
+     * @return {Rectangle} Clone.
+     */
+    clone() {
+        var topLeft = this.getUpperLeftCorner();
+        return new Rectangle(
+            topLeft[0], topLeft[1], this.getWidth(), this.getHeight(),
+            this.getTransform());
+    }
+
+    /**
+     * Returns the length of the rectangle
+     *
+     * @return {number} the length of the rectangle
+     */
+    getLength() {
+        return getLength(this);
+    }
 }
 
 export default Rectangle;
