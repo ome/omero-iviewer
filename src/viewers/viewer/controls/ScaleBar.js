35b809e4bb826a6f989bea30d504edae2806ece9
William Moore <w.moore@dundee.ac.uk>
2019-01-16 04:58:03 +0000

    Refactor controls/ScaleBar.js to ES6 class syntax

--- src/viewers/viewer/controls/ScaleBar.js
+++ src/viewers/viewer/controls/ScaleBar.js
@@ -18,118 +18,117 @@
 
 import ScaleLine from 'ol/control/ScaleLine';
 import PluggableMap from 'ol/PluggableMap';
-import {inherits} from 'ol/util';
 import {listen, unlistenByKey} from 'ol/events';
 import {UNITS_LENGTH} from '../globals';
 
 /**
  * @classdesc
  * Extends the built ScaleLine
- *
- * @constructor
- * @extends {ol.control.ScaleLine}
- * @param {Object} opt_options optional options
  */
-const ScaleBar = function(opt_options) {
-    if (typeof(opt_options) !== 'object') opt_options = {};
+class ScaleBar extends ScaleLine {
 
     /**
-     * default scale bar width in pixels
-     * @type {number}
-     * @private
+     * @constructor
+     * @param {Object} opt_options optional options
      */
-    this.bar_width_ = 100;
+    constructor(opt_options) {
+        if (typeof(opt_options) !== 'object') opt_options = {};
+        super(opt_options);
 
-    /**
-     * the scalebar 'drag' listener
-     * @type {number}
-     * @private
-     */
-    this.drag_listener_ = null;
+        /**
+         * default scale bar width in pixels
+         * @type {number}
+         * @private
+         */
+        this.bar_width_ = 100;
 
-    // goog.base(this, opt_options);
-    ScaleLine.call(this, opt_options);
+        /**
+         * the scalebar 'drag' listener
+         * @type {number}
+         * @private
+         */
+        this.drag_listener_ = null;
 
-    // give element a tooltip
-    this.element.title = "Click and drag to move scalebar";
-    // append ol-control
-    this.element.className += " ol-control";
+        // give element a tooltip
+        this.element.title = "Click and drag to move scalebar";
+        // append ol-control
+        this.element.className += " ol-control";
 
-    // register 'drag' listener
-    listen(
-        this.element, "mousedown",
-        function(start) {
-            if (!(this.map_ instanceof PluggableMap)) return;
-            if (this.drag_listener_ !== null) {
-                unlistenByKey(this.drag_listener_);
-                this.drag_listener_ = null;
-            }
-            var offsetX = -start.offsetX;
-            var offsetY = -start.offsetY;
-            this.drag_listener_ =
-                listen(this.map_, "pointermove",
-                    function(move) {
-                        var e = move.originalEvent;
-                        if (!((typeof e.buttons === 'undefined' &&
-                            e.which === 1) || e.buttons === 1)) {
-                                unlistenByKey(this.drag_listener_);
-                                this.drag_listener_ = null;
-                                return;
-                            }
-                        this.element.style.bottom = "auto";
-                        this.element.style.left =
-                            (move.pixel[0] + offsetX) + "px";
-                        this.element.style.top =
-                            (move.pixel[1] + offsetY) + "px";
-                    }, this);
-     }, this);
-}
-inherits(ScaleBar, ScaleLine);
-
-/**
- * Overridden to deal with mere pixel to unit issues instead of geographic
- * projections
- * @private
- */
-ScaleBar.prototype.updateElement_ = function() {
-  var viewState = this.viewState_;
-
-  if (!viewState) {
-    if (this.renderedVisible_) {
-      this.element.style.display = 'none';
-      this.renderedVisible_ = false;
+        // register 'drag' listener
+        listen(
+            this.element, "mousedown",
+            function(start) {
+                if (!(this.map_ instanceof PluggableMap)) return;
+                if (this.drag_listener_ !== null) {
+                    unlistenByKey(this.drag_listener_);
+                    this.drag_listener_ = null;
+                }
+                var offsetX = -start.offsetX;
+                var offsetY = -start.offsetY;
+                this.drag_listener_ =
+                    listen(this.map_, "pointermove",
+                        function(move) {
+                            var e = move.originalEvent;
+                            if (!((typeof e.buttons === 'undefined' &&
+                                e.which === 1) || e.buttons === 1)) {
+                                    unlistenByKey(this.drag_listener_);
+                                    this.drag_listener_ = null;
+                                    return;
+                                }
+                            this.element.style.bottom = "auto";
+                            this.element.style.left =
+                                (move.pixel[0] + offsetX) + "px";
+                            this.element.style.top =
+                                (move.pixel[1] + offsetY) + "px";
+                        }, this);
+        }, this);
     }
-    return;
-  }
 
-  var micronsPerPixel = viewState.projection.getMetersPerUnit();
-  var resolution = viewState.resolution;
-  var scaleBarLengthInUnits = micronsPerPixel * this.bar_width_ * resolution;
-  var symbol = '\u00B5m';
-  for (var u=0;u<UNITS_LENGTH.length;u++) {
-      var unit = UNITS_LENGTH[u];
-      if (scaleBarLengthInUnits < unit.threshold) {
-          scaleBarLengthInUnits *= unit.multiplier;
-          symbol = unit.symbol;
-          break;
-      }
-  }
+    /**
+     * Overridden to deal with mere pixel to unit issues instead of geographic
+     * projections
+     * @private
+     */
+    updateElement_() {
+        var viewState = this.viewState_;
 
-  var html = scaleBarLengthInUnits.toFixed(2) + ' ' + symbol;
-  if (this.renderedHTML_ !== html) {
-    this.innerElement_.innerHTML = html;
-    this.renderedHTML_ = html;
-  }
+        if (!viewState) {
+            if (this.renderedVisible_) {
+            this.element.style.display = 'none';
+            this.renderedVisible_ = false;
+            }
+            return;
+        }
 
-  if (this.renderedWidth_ != this.bar_width_) {
-    this.innerElement_.style.width = this.bar_width_ + 'px';
-    this.renderedWidth_ = this.bar_width_;
-  }
+        var micronsPerPixel = viewState.projection.getMetersPerUnit();
+        var resolution = viewState.resolution;
+        var scaleBarLengthInUnits = micronsPerPixel * this.bar_width_ * resolution;
+        var symbol = '\u00B5m';
+        for (var u=0;u<UNITS_LENGTH.length;u++) {
+            var unit = UNITS_LENGTH[u];
+            if (scaleBarLengthInUnits < unit.threshold) {
+                scaleBarLengthInUnits *= unit.multiplier;
+                symbol = unit.symbol;
+                break;
+            }
+        }
 
-  if (!this.renderedVisible_) {
-    this.element.style.display = '';
-    this.renderedVisible_ = true;
-  }
-};
+        var html = scaleBarLengthInUnits.toFixed(2) + ' ' + symbol;
+        if (this.renderedHTML_ !== html) {
+            this.innerElement_.innerHTML = html;
+            this.renderedHTML_ = html;
+        }
+
+        if (this.renderedWidth_ != this.bar_width_) {
+            this.innerElement_.style.width = this.bar_width_ + 'px';
+            this.renderedWidth_ = this.bar_width_;
+        }
+
+        if (!this.renderedVisible_) {
+            this.element.style.display = '';
+            this.renderedVisible_ = true;
+        }
+    }
+}
 
 export default ScaleBar;
