28bf6e1965c8034a4dd6bfe4306982880ad495ff
William Moore <w.moore@dundee.ac.uk>
2019-01-22 06:20:17 +0000

    Remove ol3-viewer docs build from generate_docs.sh

--- generate_docs.sh
+++ generate_docs.sh
@@ -6,19 +6,13 @@ echo "-----------------------------------"
 echo "Fetching Documentation Libraries..."
 echo "-----------------------------------"
 echo ""
-npm install jsdoc esdoc@0.4.8 esdoc-es7-plugin
+npm install esdoc@0.4.8 esdoc-es7-plugin
 
 # clean any docs that might have existed before and recreate the dir
 rm -rf ./build/docs
 mkdir ./build/docs
 
-# generate docs from source for both ol3viewer and iviewer
-echo ""
-echo "--------------------------------"
-echo "Running jsdocs for ol3-viewer..."
-echo "--------------------------------"
-echo ""
-./node_modules/.bin/jsdoc -r ./ol3-viewer -d ./build/docs/ol3-viewer -p --verbose
+# generate docs...
 echo ""
 echo "----------------------------"
 echo "Running esdoc for iviewer..."
