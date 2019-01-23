ccaca60980ddc26b0529aa7737c72f19aaa90802
William Moore <w.moore@dundee.ac.uk>
2019-01-23 08:11:59 +0000

    Update test/test.js to import from ../src/

--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
 
+// This file is a webpack entry point so we can
+// import from src
+import Viewer from '../src/viewers/viewer/Viewer.js';
+
 describe("Viewer", function() {
 
   it('isNotUndefined', function() {
-      var Viewer = openlayers_viewer.default;
       assert.notEqual(Viewer, undefined);
   });
 
   it('canBeCreatedWithNoId', function() {
-    var Viewer = openlayers_viewer.default;
     var v = new Viewer();
     assert.equal(v.id_, -1);
   });
