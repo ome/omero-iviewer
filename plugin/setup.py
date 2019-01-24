d9cbdf476741bf8381812df4149df741588290d2
William Moore <w.moore@dundee.ac.uk>
2019-01-24 02:24:41 +0000

    Remove unused 'prepare-unit-tests' build target

--- plugin/setup.py
+++ plugin/setup.py
@@ -78,7 +78,7 @@ class Install(setuptools.command.install.install):
 class Test(setuptools.command.install.install):
 
     def run(self):
-        self.spawn(['ant', 'unit-tests-only'])
+        self.spawn(['ant', 'unit-tests'])
         pass
 
 
