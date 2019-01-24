7b06cfb7595a667ff97f973fe086730cf0a96bdd
William Moore <w.moore@dundee.ac.uk>
2019-01-24 02:40:51 +0000

    Explicitly list test files in karma.conf.js
    Try to understand why build fails on travis

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,13 @@ module.exports = function(config) {
     ],
     files: [
       'test/test.js',
-      'test/unit/*.js',
+      'test/unit/conversion.js',
+      'test/unit/geometries.js',
+      'test/unit/misc.js',
+      'test/unit/net.js',
+      'test/unit/regions.js',
+      'test/unit/rois.js',
+      'test/unit/style.js',
     ],
     preprocessors: {
       'test/**/*.js': ['webpack']
