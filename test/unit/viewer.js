d58cff75790d9c38d60581dd21773122d224b83f
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-01-29 11:45:55 +0000

    fix path

--- test/unit/viewer.js
+++ test/unit/viewer.js
@@ -18,7 +18,7 @@
 
 // This file is a webpack entry point so we can
 // import from src
-import Viewer from '../src/viewers/viewer/Viewer.js';
+import Viewer from '../../src/viewers/viewer/Viewer.js';
 
 describe("Viewer", function() {
 
