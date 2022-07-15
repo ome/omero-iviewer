70fb15c2243f262acbb1c707bf7558e609828675
barrettMCW <103449618+barrettMCW@users.noreply.github.com>
2022-07-15 07:01:16 -0500

    rm whitespace

--- plugin/omero_iviewer/iviewer_settings.py
+++ plugin/omero_iviewer/iviewer_settings.py
@@ -40,7 +40,7 @@ IVIEWER_SETTINGS_MAPPING = {
          500,
          int,
          "Page size for ROI pagination."],
-    
+
     "omero.web.iviewer.color_palette":
         ["COLOR_PALETTE",
          '',
